0bd477472069011b9f7d0123e42b70e086db611c
William Foleis Mian <william_mian@yahoo.com.br>
2020-12-27 23:06:31 -0200

    find

--- src/main/java/br/com/alura/spring/data/repository/FuncionarioRepository.java
+++ src/main/java/br/com/alura/spring/data/repository/FuncionarioRepository.java
@@ -13,5 +13,11 @@ public interface FuncionarioRepository extends CrudRepository<Funcionario, Integ
 	//derived query
 	List<Funcionario> findByNome(String nome);
 	List<Funcionario> findByNomeLike(String nome);
+	List<Funcionario> findByNomeEndingWith(String nome);
+	List<Funcionario> findByNomeStartingWith(String nome);
+	List<Funcionario> findByNomeIsNull();
+	List<Funcionario> findByNomeIsNotNull();
+	List<Funcionario> findByNomeOrderByNomeAsc(String nome);
+	
 	
 }
