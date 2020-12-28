c51997b89a1eba69da811b3553b67fb599e5c3c3
William Foleis Mian <william_mian@yahoo.com.br>
2020-12-27 22:55:44 -0200

    derived query

--- src/main/java/br/com/alura/spring/data/SpringDataApplication.java
+++ src/main/java/br/com/alura/spring/data/SpringDataApplication.java
@@ -9,6 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import br.com.alura.spring.data.service.CrudCargoService;
 import br.com.alura.spring.data.service.CrudFuncionarioService;
 import br.com.alura.spring.data.service.CrudUnidadeTrabalhoService;
+import br.com.alura.spring.data.service.RelatoriosService;
 
 @SpringBootApplication
 public class SpringDataApplication implements CommandLineRunner {
@@ -20,13 +21,17 @@ public class SpringDataApplication implements CommandLineRunner {
 	private final CrudFuncionarioService funcionarioService;
 
 	private final CrudUnidadeTrabalhoService unidadeTrabalhoService;
+	
+	private final RelatoriosService relatoriosService;
 
 	public SpringDataApplication(CrudCargoService cargoService, 
 			CrudFuncionarioService funcionarioService, 
-			CrudUnidadeTrabalhoService unidadeTrabalhoService) {
+			CrudUnidadeTrabalhoService unidadeTrabalhoService,
+			RelatoriosService relatoriosService) {
 		this.cargoService = cargoService;
 		this.funcionarioService = funcionarioService;
 		this.unidadeTrabalhoService = unidadeTrabalhoService;
+		this.relatoriosService = relatoriosService;
 	}
 	
 	public static void main(String[] args) {
@@ -43,6 +48,7 @@ public class SpringDataApplication implements CommandLineRunner {
 			System.out.println("1 - Cargo");
 			System.out.println("2 - Funcionario");
 			System.out.println("3 - Unidade");
+			System.out.println("4 - Relatorios");
 			
 			int action = scanner.nextInt();
 			
@@ -55,6 +61,9 @@ public class SpringDataApplication implements CommandLineRunner {
 				break;
 			case 3:
 				unidadeTrabalhoService.inicial(scanner);
+				break;
+			case 4:
+				relatoriosService.inicial(scanner);
 				break;
 			default:
 				System.out.println("Finalizando");
--- src/main/java/br/com/alura/spring/data/repository/FuncionarioRepository.java
+++ src/main/java/br/com/alura/spring/data/repository/FuncionarioRepository.java
@@ -1,5 +1,7 @@
 package br.com.alura.spring.data.repository;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,8 @@ import br.com.alura.spring.data.orm.Funcionario;
 
 @Repository
 public interface FuncionarioRepository extends CrudRepository<Funcionario, Integer> {
+	
+	//derived query
+	List<Funcionario> findByNome(String nome);
 	
 }
