12366594fd833b123c02022bf3209f20e9cf4c30
William Foleis Mian <william_mian@yahoo.com.br>
2020-12-27 23:55:34 -0200

    jpql

--- src/main/java/br/com/alura/spring/data/repository/FuncionarioRepository.java
+++ src/main/java/br/com/alura/spring/data/repository/FuncionarioRepository.java
@@ -1,7 +1,9 @@
 package br.com.alura.spring.data.repository;
 
+import java.time.LocalDate;
 import java.util.List;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -18,6 +20,11 @@ public interface FuncionarioRepository extends CrudRepository<Funcionario, Integ
 	List<Funcionario> findByNomeIsNull();
 	List<Funcionario> findByNomeIsNotNull();
 	List<Funcionario> findByNomeOrderByNomeAsc(String nome);
+	List<Funcionario> findByNomeAndSalarioGreaterThanAndDataContratacao(String nome, Double salario, LocalDate dataContratacao);
 	
+	//JPQL
+	@Query("SELECT f FROM Funcionario f WHERE f.nome = :nome "
+			+ "AND f.salario >= :salario AND f.dataContratacao = :data")
+	List<Funcionario> findNomeSalarioMaiorDataContratacao(String nome, Double salario, LocalDate data);
 	
 }
