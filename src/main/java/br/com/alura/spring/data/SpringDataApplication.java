fc663c9a816d097b0a931590e667d3d10ddf09dc
William Foleis Mian <william_mian@yahoo.com.br>
2020-12-29 17:42:54 -0200

    Specifications

--- src/main/java/br/com/alura/spring/data/SpringDataApplication.java
+++ src/main/java/br/com/alura/spring/data/SpringDataApplication.java
@@ -9,6 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import br.com.alura.spring.data.service.CrudCargoService;
 import br.com.alura.spring.data.service.CrudFuncionarioService;
 import br.com.alura.spring.data.service.CrudUnidadeTrabalhoService;
+import br.com.alura.spring.data.service.RelatorioFuncionarioDinamico;
 import br.com.alura.spring.data.service.RelatoriosService;
 
 @SpringBootApplication
@@ -23,15 +24,19 @@ public class SpringDataApplication implements CommandLineRunner {
 	private final CrudUnidadeTrabalhoService unidadeTrabalhoService;
 	
 	private final RelatoriosService relatoriosService;
+	
+	private final RelatorioFuncionarioDinamico relatorioFuncionarioDinamico;
 
 	public SpringDataApplication(CrudCargoService cargoService, 
 			CrudFuncionarioService funcionarioService, 
 			CrudUnidadeTrabalhoService unidadeTrabalhoService,
-			RelatoriosService relatoriosService) {
+			RelatoriosService relatoriosService, 
+			RelatorioFuncionarioDinamico relatorioFuncionarioDinamico) {
 		this.cargoService = cargoService;
 		this.funcionarioService = funcionarioService;
 		this.unidadeTrabalhoService = unidadeTrabalhoService;
 		this.relatoriosService = relatoriosService;
+		this.relatorioFuncionarioDinamico = relatorioFuncionarioDinamico;
 	}
 	
 	public static void main(String[] args) {
@@ -49,6 +54,7 @@ public class SpringDataApplication implements CommandLineRunner {
 			System.out.println("2 - Funcionario");
 			System.out.println("3 - Unidade");
 			System.out.println("4 - Relatorios");
+			System.out.println("5 - Relatorio dinamico");
 			
 			int action = scanner.nextInt();
 			
@@ -65,6 +71,10 @@ public class SpringDataApplication implements CommandLineRunner {
 			case 4:
 				relatoriosService.inicial(scanner);
 				break;
+			case 5:
+				relatorioFuncionarioDinamico.inicial(scanner);
+				break;
+				
 			default:
 				System.out.println("Finalizando");
 				system = false;
--- src/main/java/br/com/alura/spring/data/repository/FuncionarioRepository.java
+++ src/main/java/br/com/alura/spring/data/repository/FuncionarioRepository.java
@@ -3,6 +3,7 @@ package br.com.alura.spring.data.repository;
 import java.time.LocalDate;
 import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
@@ -12,7 +13,8 @@ import br.com.alura.spring.data.orm.FuncionarioDto;
 import br.com.alura.spring.data.orm.FuncionarioProjecao;
 
 @Repository
-public interface FuncionarioRepository extends PagingAndSortingRepository<Funcionario, Integer> {
+public interface FuncionarioRepository extends PagingAndSortingRepository<Funcionario, Integer>,
+											JpaSpecificationExecutor<Funcionario> {
 	
 	//derived query
 	List<Funcionario> findByNome(String nome);
